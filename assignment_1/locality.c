a3c0987488e2b806771397a4e0c091211be1d74a
Stefano Ribes <stefanor@ribes.got.gaisler.com>
2022-07-08 17:46:59 +0200

    Assignment1: Starting working on Locality

--- assignment_1/locality.c
+++ assignment_1/locality.c
@@ -2,55 +2,77 @@
 #include <stdlib.h>
 #include <time.h>
 
+void row_sums(double* sums, const double** matrix, size_t nrs, size_t ncs) {
+  for (size_t ix = 0; ix < nrs; ++ix) {
+    double sum = 0.;
+    for (size_t jx = 0; jx < ncs; ++jx) {
+      sum += matrix[ix][jx];
+    }
+    sums[ix] = sum;
+  }
+}
+
+void col_sums(double* sums, const double** matrix, size_t nrs, size_t ncs) {
+  for (size_t jx = 0; jx < ncs; ++jx) {
+    double sum = 0.;
+    for (size_t ix = 0; ix < nrs; ++ix) {
+      sum += matrix[ix][jx];
+    }
+    sums[jx] = sum;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   /**
-   Write a program, called inlined_manually, that
+   Write a C program, called locality, that
 
-   * generates vectors as_re, as_im, bs_re, bs_im, cs_im, and cs_im of doubles,
-     each of length 30,000,
-   * generates entries for bs_re, bs_im, cs_re, and cs_im (any kind of entries
-     will do), and then
-   * multiplies the entries of bs and cs saving results to as by inserting the
-     implementation of mul_cpx into the main function by hand.
-   * print one random entry of as to the terminal.
+   * creates 1000 x 1000 doubles,
+   * stores them in a matrix in row major order,
+   * computes the row and the column sums.
   */
   srand(time(NULL));
-  int num_elem = 30000;
-  int num_tests = 200000;
-  double* as_re = (double*) malloc(sizeof(double) * num_elem);
-  double* as_im = (double*) malloc(sizeof(double) * num_elem);
-  double* bs_re = (double*) malloc(sizeof(double) * num_elem);
-  double* bs_im = (double*) malloc(sizeof(double) * num_elem);
-  double* cs_re = (double*) malloc(sizeof(double) * num_elem);
-  double* cs_im = (double*) malloc(sizeof(double) * num_elem);
-  // Initialize vectors
-  for (int i = 0; i < num_elem; ++i) {
-    bs_re[i] = rand();
-    bs_im[i] = rand();
-    cs_re[i] = rand();
-    cs_im[i] = rand();
+  const int size = 1000;
+  const int num_tests = 5000;
+  double* asentries = (double*) malloc(sizeof(double) * size * size);
+  double** as = (double**) malloc(sizeof(double*) * size);
+  double* rsums = (double*) malloc(sizeof(double) * size);
+  double* csums = (double*) malloc(sizeof(double) * size);
+  for (size_t ix = 0, jx = 0; ix < size; ++ix, jx += size) {
+    as[ix] = asentries + jx;
+  }
+  for (size_t ix = 0; ix < size; ++ix) {
+    for (size_t jx = 0; jx < size; ++jx) {
+      as[ix][jx] = rand();
+    }
   }
   // Start computation
+  // Rows
   clock_t start = clock();
   for (int t = 0; t < num_tests; ++t) {
-    for (int i = 0; i < num_elem; ++i) {
-      // NOTE: (a + ib) * (c + di) = (ac - bd) + i (ad + bc)
-      as_re[i] = bs_re[i] * cs_re[i] - bs_im[i] * cs_im[i];
-      as_im[i] = bs_re[i] * cs_im[i] + bs_im[i] * cs_re[i];
-    }
+    row_sums(rsums, (const double**)as, size, size);
   }
   clock_t end = clock();
   float tot_time = (float)(end - start) / CLOCKS_PER_SEC;
   // Printing
-  int rand_idx = rand() % num_elem;
-  printf("Random element: %f + %f i\n", as_re[rand_idx], as_im[rand_idx]);
-  printf("Total time:     %f\n", tot_time);
-  printf("Time per iter:  %f\n", tot_time / (float) (num_elem));
-  free(as_re);
-  free(as_im);
-  free(bs_re);
-  free(bs_im);
-  free(cs_re);
-  free(cs_im);
+  printf("[ROWS] Random element: %d\n", rsums[rand() % size]);
+  printf("[ROWS] Total time:     %f [s]\n", tot_time);
+  printf("[ROWS] Time per iter:  %f [s]\n", tot_time / (float) (num_tests));
+  // Columns
+  start = clock();
+  for (int t = 0; t < num_tests; ++t) {
+    col_sums(csums, (const double**)as, size, size);
+  }
+  end = clock();
+  tot_time = (float)(end - start) / CLOCKS_PER_SEC;
+  // Printing
+  printf("\n");
+  printf("[COLS] Random element: %d\n", csums[rand() % size]);
+  printf("[COLS] Total time:     %f [s]\n", tot_time);
+  printf("[COLS] Time per iter:  %f [s]\n", tot_time / (float) (num_tests));
+  // Free memory
+  free(as);
+  free(asentries);
+  free(rsums);
+  free(csums);
   return 0;
 }
