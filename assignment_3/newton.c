135ae11879197f686422e2bce97f8c8e6196a8b6
Stefano Ribes <stefanor@ribes.got.gaisler.com>
2022-09-12 18:49:22 +0200

    Assignment3: Started working on multithreading

--- assignment_3/newton.c
+++ assignment_3/newton.c
@@ -9,6 +9,8 @@
 #include <math.h>
 #include <getopt.h>
 #include <complex.h>
+#include <threads.h>
+#include <stdatomic.h>
 
 /*
  * Global variables (to make them available to all threads)
@@ -18,6 +20,8 @@ int num_lines = -1;
 int num_attractor = -1;
 int degree = -1;
 double complex* roots = NULL;
+atomic_bool computation_done = false;
+atomic_int_least16_t next_idx = 0;
 
 const int kIterCutoff = 100;
 const double kEpsilon = 1e-3;
@@ -26,7 +30,86 @@ const double kDivMaxRe = 1e10;
 const double kDivMinIm = -1e10;
 const double kDivMaxIm = 1e10;
 const double kPi = 22.0 / 7.0;
+const int kBlockSize = 100;
 static const double kNaN = 0.0 / 0.0; // NOTE: NAN should be avail from math.h
+
+typedef struct {
+  mtx_t mtx;
+  cnd_t cnd;
+  size_t cnt;
+} smphr_t;
+
+typedef struct {
+  int id;
+  smphr_t* idx_smphr;
+  smphr_t* thrd_smphr;
+} thrd_args_t;
+
+int smphr_init(const size_t count, smphr_t* smphr) {
+  smphr->cnt = count;
+  int r = mtx_init(&smphr->mtx, mtx_plain);
+  if (r == thrd_success) {
+    r = cnd_init(&smphr->cnd);
+  }
+  return r;
+}
+
+void smphr_destroy(smphr_t* smphr) {
+  mtx_destroy(&smphr->mtx);
+  cnd_destroy(&smphr->cnd);
+}
+
+void smphr_release(smphr_t* smphr) {
+  mtx_lock(&smphr->mtx);
+  ++smphr->cnt;
+  cnd_signal(&smphr->cnd);
+  mtx_unlock(&smphr->mtx);
+}
+
+void smphr_release_all(const size_t count, smphr_t* smphr) {
+  // TODO: Double check the use of this condition. Maybe an extra one is needed
+  mtx_lock(&smphr->mtx);
+  while (smphr->cnt != 0) {
+    cnd_wait(&smphr->cnd, &smphr->mtx);
+  }
+  smphr->cnt = count;
+  cnd_signal(&smphr->cnd);
+  mtx_unlock(&smphr->mtx);
+}
+
+int smphr_acquire(smphr_t* smphr) {
+  mtx_lock(&smphr->mtx);
+  while (smphr->cnt == 0) {
+    cnd_wait(&smphr->cnd, &smphr->mtx);
+  }
+  int idx = kBlockSize - smphr->cnt;
+  if (atomic_load_explicit(&computation_done, memory_order_relaxed)) {
+    idx = -1;
+  }
+  --smphr->cnt;
+  mtx_unlock(&smphr->mtx);
+  return idx;
+}
+
+int compute_thread(void* args_in) {
+  thrd_args_t* args = (thrd_args_t*)args_in;
+  printf("Hello from thread n.%d\n", args->id);
+  int idx = smphr_acquire(args->idx_smphr);
+  while(!atomic_load_explicit(&computation_done, memory_order_relaxed)) {
+
+    // Simulate some processing...
+    printf("Thread n.%d acquired index n.%d\n", args->id, idx);
+    thrd_sleep(&(struct timespec){.tv_sec=0, .tv_nsec=50000}, NULL);
+
+    idx = smphr_acquire(args->idx_smphr);
+    if (idx < 0) {
+      printf("Thread n.%d returning.\n", args->id);
+      return 0;
+    }
+  }
+  printf("Thread n.%d returning.\n", args->id);
+  return 0;
+}
 
 // TODO: Be careful about alignment!!!
 typedef struct attractor_t {
@@ -46,13 +129,14 @@ attractor_t rootiter(double complex z) {
   attractor_t ret;
   ret.attr = (int)NAN;
   ret.iter = 0;
-  // while(ret.iter < 100) {
-  while(true) {
+  // while(true) {
+  while(ret.iter < 50) {
     if (creal(z) < kDivMinRe || creal(z) > kDivMaxRe ||
         cimag(z) < kDivMinIm || cimag(z) > kDivMaxIm) {
       ret.attr = degree;
       break;
     }
+    // TODO: The use of cabs() can be avoided.
     if (cabs(z) < kEpsilon) {
       ret.attr = degree + 1;
     }
@@ -67,6 +151,8 @@ attractor_t rootiter(double complex z) {
     if (ret.attr != (int)NAN) {
       break;
     }
+    // TODO: The function cpow() must be removed. A switch-case on the degree
+    // can help improving/optimizing the computation
     z = z * (1. - 1. / degree) + 1. / cpow(z, (degree - 1)) / degree;
     ret.iter++;
   }
@@ -81,7 +167,7 @@ int main(int argc, char* const* argv) {
    */
   srand(time(NULL));
   int opt;
-  // NOTE: The colon after the argument makes it a "required" optinal argument
+  // NOTE: The colon after the argument makes it a "required" optional argument
   while ((opt = getopt(argc, argv, "t:l:")) != -1) {
     switch (opt) {
     case 't':
@@ -104,6 +190,10 @@ int main(int argc, char* const* argv) {
     fprintf(stderr, "ERROR. Degree must be positive. Supplied: %d. Exiting\n", degree);
     exit(EXIT_FAILURE);
   }
+  if (degree >= 10) {
+    fprintf(stderr, "ERROR. Degree must be less than 10. Supplied: %d. Exiting\n", degree);
+    exit(EXIT_FAILURE);
+  }
   if (num_lines == -1) {
     fprintf(stderr, "ERROR. Number of lines must be supplied. Usage: %s [-t N] -l L d\n", argv[0]);
     exit(EXIT_FAILURE);
@@ -112,6 +202,50 @@ int main(int argc, char* const* argv) {
     fprintf(stderr, "ERROR. Number of lines must be positive. Supplied: %d. Exiting\n", num_lines);
     exit(EXIT_FAILURE);
   }
+  if (num_lines >= 100000) {
+    fprintf(stderr, "ERROR. Number of lines must be less than 100,000. Supplied: %d. Exiting\n", num_lines);
+    exit(EXIT_FAILURE);
+  }
+
+  int r;
+  smphr_t idx_smphr;
+  smphr_init(kBlockSize, &idx_smphr);
+
+  thrd_t* compute_threads = (thrd_t*)malloc(num_threads * sizeof(thrd_t));
+  thrd_args_t* thrd_args = (thrd_args_t*)malloc(num_threads * sizeof(thrd_args_t));
+
+  for (int tx = 0; tx < num_threads; ++tx) {
+    thrd_args[tx].id = tx;
+    thrd_args[tx].idx_smphr = &idx_smphr;
+    thrd_args[tx].thrd_smphr = &thrd_smphr;
+    int r = thrd_create(&compute_threads[tx], compute_thread, (void*)(&thrd_args[tx]));
+    if (r != thrd_success) {
+      fprintf(stderr, "ERROR. Failed to create thread %d. Exiting.\n", tx);
+      exit(1);
+    }
+    // thrd_detach(compute_threads[tx]);
+  }
+
+
+  thrd_sleep(&(struct timespec){.tv_sec=1, .tv_nsec=1000}, NULL);
+  printf("Main thread freeing compute threads...\n");
+
+
+  // printf("Releasing semaphores...\n");
+  atomic_store(&computation_done, true);
+  smphr_release_all(kBlockSize, &idx_smphr);
+  cnd_broadcast(&(idx_smphr.cnd));
+
+  for (int tx = 0; tx < num_threads; ++tx) {
+    thrd_join(compute_threads[tx], &r);
+  }
+
+  smphr_destroy(&idx_smphr);
+
+  free(compute_threads);
+  free(thrd_args);
+
+#if 0
   /*
    * Precompute roots
    */
@@ -170,6 +304,7 @@ int main(int argc, char* const* argv) {
       int iter = attr_iters[i][j].iter;
       int attr1 = (attr + 1 * num_attractor / 3) % num_attractor;
       int attr2 = (attr + 2 * num_attractor / 3) % num_attractor;
+      // TODO: The function fprintf() is slow, prefer using fwrite().
       fprintf(attr_fp, "%d %d %d ", attr, attr1, attr2);
       fprintf(iter_fp, "%d %d %d ", iter, iter, iter);
     }
@@ -184,4 +319,5 @@ int main(int argc, char* const* argv) {
   free(roots);
   free(attr_iters_entries);
   free(attr_iters);
+#endif
 }
